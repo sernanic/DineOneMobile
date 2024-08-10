fbddebf46de14172dbf5f896af8aa653b0248f68
Nicolas Serna <sernan100@gmail.com>
2024-08-10 13:49:36 -0400

    feat: custom drawer implementation started

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,19 +4,31 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ApplicationProvider } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
-
+import { Ionicons } from '@expo/vector-icons';
+import CustomDrawerContent from '@/components/drawer/customDrawer'
+import Colors from '../constants/Colors';
 export default function Layout() {
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <BottomSheetModalProvider>
-          <Drawer>
+          <Drawer drawerContent={CustomDrawerContent}
+          screenOptions={
+            {drawerHideStatusBarOnOpen:true,
+              drawerActiveBackgroundColor:Colors.primary,
+              drawerActiveTintColor:'#fff'
+
+          }}
+          >
             <Drawer.Screen
               name="(tabs)"
               options={{
                 drawerLabel: "Home",
                 title: "Home",
                 headerShown: false,
+                drawerIcon:({size,color}) =>{
+                  return <Ionicons name="settings-outline" size={size} color={color}/>
+                }
               }}
             />
             {/* <Drawer.Screen
---
+++ components/drawer/customDrawer.tsx
@@ -0,0 +1,76 @@
+// CustomDrawerContent.tsx
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import { useNavigation, useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+
+const CustomDrawerContent: React.FC<DrawerContentComponentProps> = (props) => {
+    const navigation = useNavigation();
+    const router = useRouter()
+    const { top, bottom } = useSafeAreaInsets();
+
+    return (
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props} scrollEnabled={false}>
+                <DrawerItemList {...props} />
+                <DrawerItem label={'Logout'} onPress={() => router.replace('/')} />
+
+            </DrawerContentScrollView>
+            <View style={{
+                borderTopColor: 'red',
+                borderTopWidth: 1,
+                padding: 20,
+                paddingBottom: 20 + bottom
+            }}>
+                <Text style={{ color: 'blue' }}>Footer</Text>
+
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#fff',
+    },
+    profileSection: {
+        marginBottom: 20,
+    },
+    profileText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    drawerItems: {
+        flex: 1,
+    },
+    drawerItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    drawerItemText: {
+        fontSize: 16,
+        marginLeft: 10,
+    },
+    logoutButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    logoutText: {
+        fontSize: 16,
+        marginLeft: 10,
+        color: Colors.primary,
+    },
+    footer: {
+
+    }
+});
+
+export default CustomDrawerContent;
